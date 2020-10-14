0292eae9ff9d662beba1ecfb9d83df3c21cf0289
hyejin-s <parkebbi2@gmail.com>
2020-10-15 02:45:18 +0900

    L_beta graph

--- plot_txt.py
+++ plot_txt.py
@@ -1,17 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import re
-M = 200
-smooth = 5
-valuelist = [[0.05, 0.05], [0.2, 0.05], [0.05, 0.5], [0.2, 0.5]]
+M = 100
+smooth = 1
 filepath = r"C:\Users\parke\bandit\checking"
-
-colors = ['tomato','limegreen', 'dodgerblue', 'gold']
 
 with open(r"C:\Users\parke\bandit\checking\regret_transfer.txt") as f:
     data = f.read().split()
     plt.figure()
-    X = np.linspace(0, 1, M/smooth)
+    X = np.linspace(0, 1, int(M/smooth))
+    colors = ['tomato', 'orangered','limegreen', 'dodgerblue']
     for i in range(4):
         regret_list = []
         for m in range(M):
@@ -23,15 +21,15 @@ with open(r"C:\Users\parke\bandit\checking\regret_transfer.txt") as f:
         for j in range(int(M/smooth)):
             Y_list.append(np.sum(regret_list[j*smooth:(j+1)*smooth])/smooth)
             
-        plt.plot(X, Y_list, color=colors[i], label="{}".format(valuelist[i]))
+        plt.plot(X, Y_list, color=colors[i], label="{}".format(i))
     plt.legend()
-    plt.savefig(filepath+'/regret_transfer_smooth{}'.format(smooth), dpi=300)
-    plt.show()
+
 
 with open(r"C:\Users\parke\bandit\checking\regret_info.txt") as f:
     data = f.read().split()
     plt.figure()
-    X = np.linspace(0, 1, M/smooth)
+    X = np.linspace(0, 1, int(M/smooth))
+    colors = ['tomato','limegreen', 'dodgerblue']
     for i in range(3):
         regret_list = []
         for m in range(M):
@@ -45,14 +43,13 @@ with open(r"C:\Users\parke\bandit\checking\regret_info.txt") as f:
             
         plt.plot(X, Y_list, color=colors[i], label="{}".format(i))
     plt.legend()
-    plt.savefig(filepath+'/regret_info_smooth{}'.format(smooth), dpi=300)
-    plt.show()
+
 
 
 with open(r"C:\Users\parke\bandit\checking\L_beta.txt") as f:
     data = f.read().split()
+    X = np.linspace(0, 1, int(M/smooth))
     plt.figure()
-    X = np.linspace(0, 1, M/smooth)
     for i in range(4):
         constant_list = []
         for m in range(M):
@@ -61,7 +58,19 @@ with open(r"C:\Users\parke\bandit\checking\L_beta.txt") as f:
         for j in range(int(M/smooth)):
             Y_list.append(np.sum(constant_list[j*smooth:(j+1)*smooth]))
             
-        plt.plot(X, Y_list, label="{}".format(valuelist[i]))
+        plt.plot(X, Y_list, label="{}".format(i))
     plt.legend()
-    plt.savefig(filepath+'/L_beta_smooth{}'.format(smooth), dpi=300)
-    plt.show()
+    plt.savefig(filepath+'/L_beta{}'.format(smooth), dpi=300)
+
+
+with open(r"C:\Users\parke\bandit\checking\EmpiricalLipschitz.txt") as f:
+    data = f.read().split()
+    for i in range(7):
+        plt.figure()
+        constant_list = []
+        for m in range(M):
+            constant_list.append(float(data[i*(M*2+1)+m*2+2]))
+            
+        plt.hist(constant_list, label="{}".format(i+1))
+        plt.legend()
+        plt.savefig(filepath+'/histogram{}'.format(i+1), dpi=300)
