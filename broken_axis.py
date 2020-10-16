f1a7ae25fe26fc42a678b875e1d8a06255db5828
hyejin-s <parkebbi2@gmail.com>
2020-10-16 13:42:50 +0900

    Drawing plot from txt file

--- broken_axis.py
+++ broken_axis.py
@@ -4,17 +4,23 @@ import numpy as np
 from cycler import cycler
 plt.rcParams['axes.prop_cycle'] = cycler(color='rbgcmyk')
 
+filepath = r"C:\Users\parke\OneDrive\바탕 화면\fig"
+
 bottom=[4.831575e+04, 4.095000e+01, 1.149900e+03, 7.100000e+00, 4.314000e+02, 5.490000e+01]
 top=[2407.9, 52.95, 66.15, 46656.3, 764.2, 52.5]
 X=[1,2,3,4,5,6] 
+X1 = []
+X2 = []
 
-
+width = 0.4 # the width of the bars
+for i in range(len(X)):
+    X1.append(X[i]+0.2)
+    X2.append(X[i]-0.2)
 
 f, (ax, ax2) = plt.subplots(2, 1, sharex=True)
 # plot the same data on both axes
-lb, = ax.plot(bottom)
-lb.set_label('bottom 20.0%')
-ax2.plot(bottom)
+ax.bar(X2, bottom, width, label="bottom 20.0%")
+ax2.bar(X2, bottom, width)
 
 ax.set_ylim(40000, 50000) # outliers only
 ax2.set_ylim(0, 10000) # most of the data
@@ -28,9 +34,8 @@ ax2.xaxis.tick_bottom()
 
 # plot the same data on both axes
 
-lt, = ax.plot(top)
-lt.set_label('top 20.0%')
-ax2.plot(top)
+ax.bar(X1, top, width, label="top 20.0%")
+ax2.bar(X1, top, width)
 
 ax.set_ylim(40000, 50000) # outliers only
 ax2.set_ylim(0, 10000) # most of the data
@@ -42,7 +47,7 @@ ax.xaxis.tick_top()
 ax.tick_params(labeltop=False)  # don't put tick labels at the top
 ax2.xaxis.tick_bottom()
 
-d = .01  # how big to make the diagonal lines in axes coordinates
+d = .015  # how big to make the diagonal lines in axes coordinates
 # arguments to pass to plot, just so we don't keep repeating them
 kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
 ax.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
@@ -52,9 +57,13 @@ kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
 ax2.plot((-d, +d), (1-d, 1+d), **kwargs)  # bottom-left diagonal
 ax2.plot((1-d, 1+d), (1-d, 1+d), **kwargs)  # bottom-right diagonal
 
-ax.legend()
+ax.legend(fontsize=12)
 
-plt.xlabel('Arm Index' )
-plt.ylabel('Number of Pulls')
+ax.set_yticklabels(['40k','','','','','50k'], fontsize=12)
+ax2.set_yticklabels([0,'','','','','10k'], fontsize=12)
+ax2.set_xticklabels(['','1','2','3','4','5','6'], fontsize=11)
+plt.xlabel('Arm Index', fontsize=20)
+plt.ylabel('                  Number of Pulls', fontsize=20)
 
-plt.show()
+plt.savefig(filepath+'/topbottom', dpi=300)
+plt.savefig(filepath+'/topbottom.pdf', format='pdf', dpi=300) 
--- plot_txt.py
+++ plot_txt.py
@@ -1,14 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import re
-M = 100
+M = 400
 smooth = 1
+value = [[0.2, 0.1], [0.2, 0.5], [0.1, 0.1], [0.1, 0.5]]
 filepath = r"C:\Users\parke\bandit\checking"
 
 with open(r"C:\Users\parke\bandit\checking\regret_transfer.txt") as f:
     data = f.read().split()
     plt.figure()
-    X = np.linspace(0, 1, int(M/smooth))
+    X = list(range(1, int(M/smooth)+1))
     colors = ['tomato', 'orangered','limegreen', 'dodgerblue']
     for i in range(4):
         regret_list = []
@@ -28,7 +29,7 @@ with open(r"C:\Users\parke\bandit\checking\regret_transfer.txt") as f:
 with open(r"C:\Users\parke\bandit\checking\regret_info.txt") as f:
     data = f.read().split()
     plt.figure()
-    X = np.linspace(0, 1, int(M/smooth))
+    X = list(range(1, int(M/smooth)+1))
     colors = ['tomato','limegreen', 'dodgerblue']
     for i in range(3):
         regret_list = []
@@ -45,10 +46,9 @@ with open(r"C:\Users\parke\bandit\checking\regret_info.txt") as f:
     plt.legend()
 
 
-
 with open(r"C:\Users\parke\bandit\checking\L_beta.txt") as f:
     data = f.read().split()
-    X = np.linspace(0, 1, int(M/smooth))
+    X = list(range(1, int(M/smooth)+1))
     plt.figure()
     for i in range(4):
         constant_list = []
@@ -58,7 +58,7 @@ with open(r"C:\Users\parke\bandit\checking\L_beta.txt") as f:
         for j in range(int(M/smooth)):
             Y_list.append(np.sum(constant_list[j*smooth:(j+1)*smooth]))
             
-        plt.plot(X, Y_list, label="{}".format(i))
+        plt.plot(X, Y_list, label="{}".format(value[i]))
     plt.legend()
     plt.savefig(filepath+'/L_beta{}'.format(smooth), dpi=300)
 
@@ -74,3 +74,16 @@ with open(r"C:\Users\parke\bandit\checking\EmpiricalLipschitz.txt") as f:
         plt.hist(constant_list, label="{}".format(i+1))
         plt.legend()
         plt.savefig(filepath+'/histogram{}'.format(i+1), dpi=300)
+
+with open(r"C:\Users\parke\bandit\checking\trueLC.txt") as f:
+    data = f.read().split()
+    X = list(range(1, int(M/smooth)+1))
+    plt.figure()
+
+    constant_list = []
+    for m in range(M):
+        constant_list.append(float(data[m*2+1]))
+            
+    plt.hist(constant_list, label="L_m")
+    plt.legend()
+    plt.savefig(filepath+'/L_m{}'.format(smooth), dpi=300)
