3273455da1e689172b3d802e27b1636ca7f1f7dc
hyejin-s <parkebbi2@gmail.com>
2020-10-16 14:31:18 +0900

    Final Generative model

--- GenerativeModel.py
+++ GenerativeModel.py
@@ -2,13 +2,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-
 class GenerativeModel(object):
     def __init__(self, num_arm):
         self.num_arm = num_arm
 
     def gene_embedding(self):
-        embeddings = list(np.linspace(0,1,self.num_arm))
+        #embeddings = list(np.linspace(0,1,self.num_arm)) # uniform
+        embeddings = [0]
+        for i in range(self.num_arm-1):
+            embeddings.append(0.9 + i* 0.1/(self.num_arm-2))
         return embeddings
 
     def gene_arms(self, LipC):
@@ -16,19 +18,28 @@ class GenerativeModel(object):
         arms[0] = random.random()
         for i in range(self.num_arm-1):
         # max(a-L/(K-1), 0), min(a+L(K-1),1)
-            arms[i+1] = random.uniform(max(arms[i]-LipC/(self.num_arm-1),0),min(arms[i]+LipC/(self.num_arm-1),1))
+            arms[i+1] = random.uniform(max(arms[i]-LipC*abs(embeddings[i+1]-embeddings[i]),0),min(arms[i]+LipC*abs(embeddings[i+1]-embeddings[i]),1))
+        for i in range(self.num_arm):
+            if arms[i] >= 0.95: 
+                arms[i] = 0.95
+            if arms[i] <= 0.05:
+                arms[i] = 0.05
         return arms
 
-"""
-a = GenerativeModel(5)
-print(a.gene_arms(10))
-print(a.gene_embedding())
-"""
+
+generation = GenerativeModel(10)
+#arms = generation.gene_arms(0.2)
+embeddings = generation.gene_embedding()
+
 
 """
 plt.figure()
-X = embeddings_gene
-Y = arms_gene
-plt.plot(embeddings_gene, arms_gene)
+plt.ylim(0, 1)
+for i in range(10):
+    generation = GenerativeModel(10) #The number of arms = 10
+    arms = generation.gene_arms(0.2) #Lipschitz Constant = 0.2
+
+    plt.plot(embeddings, arms, label="{}".format(i))
+plt.legend()
 plt.show()
 """
