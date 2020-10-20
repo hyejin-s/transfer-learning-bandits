d9064c998c87af7a12f722c36c512668a63d3f83
hyejin-s <parkebbi2@gmail.com>
2020-10-20 16:05:51 +0900

    Add plot bar version (can divided number by myself)

--- plot_cumulativeRegret_Lbeta.py
+++ plot_cumulativeRegret_Lbeta.py
@@ -69,7 +69,7 @@ with open(r"C:\Users\parke\bandit\checking\regret_transfer.txt") as f:
          
     # Make the histogram using a list of lists
     # Normalize the flights and assign colors and names
-    plt.hist([x1, x2, x3, x4, x5], bins = int(180/15), color = colors, label=labels2)
+    plt.hist([x1, x2, x3, x4, x5], bins = int(180/5), color = colors, label=labels2)
 
     # Plot formatting
     plt.legend()
@@ -87,3 +87,51 @@ plt.legend(fontsize=40)
 plt.savefig(filepath+'/cumregret', dpi=300)
 plt.savefig(filepath+'/cumregret.pdf', format='pdf', dpi=300) 
 """
+
+"""
+width = 0.4 # the width of the bars
+for i in range(len(X)):
+    X1.append(X[i]+0.2)
+    X2.append(X[i]-0.2)
+"""
+# for comparing difference value_histogram
+with open(r"C:\Users\parke\bandit\checking\regret_transfer.txt") as f:
+    data = f.read().split()
+    # for ture
+    regret_true = []
+    plt.figure()
+    for m in range(M):
+        regret_true.append(float(data[7*m+4][:-1]))
+
+    regret_difference = np.zeros((6,M))
+    plotfor = np.zeros((6,5))
+
+    for i in range(6):
+        X = []
+        if i < 3:
+            for j in range(5):
+                X.append(j-0.2+0.1*i)
+        elif i > 3:
+            for j in range(5):
+                X.append(j-0.2+0.1*(i-1))
+        if i != 3:
+            for m in range(M):
+                if data[7*m+(i+1)][-1] == ',':
+                    regret_difference[i][m] = (float(data[7*m+(i+1)][:-1])-regret_true[m])            
+                else:
+                    regret_difference[i][m] = (float(data[7*m+(i+1)])-regret_true[m])
+                
+                if regret_difference[i][m] < -250:
+                    plotfor[i][0] += 1
+                elif -250 <= regret_difference[i][m] < 0:
+                    plotfor[i][1] += 1
+                elif 0 <= regret_difference[i][m] < 250:
+                    plotfor[i][2] += 1    
+                elif 250 <= regret_difference[i][m] < 500:
+                    plotfor[i][3] += 1
+                elif regret_difference[i][m] > 500:
+                    plotfor[i][4] += 1
+            plt.bar(X, plotfor[i], width=0.1, label=labels2)
+    plt.xticks([0,1,2,3,4], ['r<-250', "-250<r<0", '0<r<250', '250<r<500', 'r>500'])
+    plt.legend()
+    plt.show()
